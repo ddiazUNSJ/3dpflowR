fc3166fef43b488ffbb14e75fb89398d8a6adea7
ddiazUNSJ <yddiaz@gmail.com>
2016-12-20 07:46:20 -0300

    segunda modif de poniendo menu principal

---
+++ imports/ui/pages/overview/overlog.js
@@ -0,0 +1,43 @@
+import './overlog.html';
+
+
+import '../../components/overview/about/about.js';
+import '../../components/overview/meetup/meetup.js';
+
+/*****************************************************************************/
+/* Overview: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Overlog.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Overlog.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Overview: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Overlog.created = function () {
+};
+
+Template.Overlog.rendered = function () {
+  $('.waypoint').waypoint(function() {
+    $(this).addClass('show');
+  }, {offset:'80%'});  
+};
+
+Template.Overlog.destroyed = function () {
+};
+
+
---
+++ imports/ui/pages/overview/overview.js
@@ -0,0 +1,43 @@
+import './overview.html';
+
+import '../../components/overview/about/about.js';
+import '../../components/overview/meetup/meetup.js';
+
+/*****************************************************************************/
+/* Overview: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Overview.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Overview.helpers({
+ 
+    /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Overview: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Overview.created = function () {
+};
+
+Template.Overview.rendered = function () {
+  $('.waypoint').waypoint(function() {
+    $(this).addClass('show');
+  }, {offset:'80%'});  
+};
+
+Template.Overview.destroyed = function () {
+};
+
+
